02c59052e14e0a754ce98112013368b96acb0174
AnnanR <everettannan@gmail.com>
2026-02-02 22:29:52 +0300

    Initial commit for AkanNameGenerator

---
+++ script.js
@@ -0,0 +1,41 @@
+document.getElementById("AkanForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const birthdate = document.getElementById("birthdate").value;
+    const gender = document.querySelector('input[name="gender"]:checked');
+
+    if (!birthdate) {
+        alert("Please select a birthdate!!");
+        return;
+    }
+
+    if (!gender) {
+        alert("Please select a gender!!");
+        return;
+    }
+
+    const date = new Date(birthdate);
+    const DD = date.getDate();
+    const MM = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    const CC = Math.floor(year / 100);
+    const YY = year % 100;
+
+    const d = Math.floor(
+        ((4 * CC - 2 * CC - 1) +
+        (45 * YY) +
+        (1026 * (MM + 1)) / 10 +
+        DD) % 7
+    );
+
+    const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    const akanName =
+        gender.value === "male" ? maleNames[d] : femaleNames[d];
+
+    document.getElementById("akan-name").innerText =
+        `You were born on a ${days[d]}. Your Akan name is ${akanName}!`;
+});
